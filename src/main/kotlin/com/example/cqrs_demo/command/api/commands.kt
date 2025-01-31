fd461cc2158323e352b14ca48d190abbe91c9a98
Haagsma <y.haagsma@portofrotterdam.com>
2025-01-31 09:27:35 +0100

    Added commands and queries

--- src/main/kotlin/com/example/cqrs_demo/command/api/commands.kt
+++ src/main/kotlin/com/example/cqrs_demo/command/api/commands.kt
@@ -1,6 +1,7 @@
 package com.example.cqrs_demo.command.api
 
 import org.axonframework.modelling.command.TargetAggregateIdentifier
+import java.math.BigDecimal
 import java.util.*
 
 data class CreateCart(
@@ -9,17 +10,28 @@ data class CreateCart(
 
 data class AddItemToCart(
     @TargetAggregateIdentifier val cartId: UUID,
-    val itemId: UUID,
+    val productId: UUID,
     val quantity: Int,
+    val basePrice: BigDecimal,
 )
 
+data class AdjustItemQuantityInCart(
+    @TargetAggregateIdentifier val cartId: UUID,
+    val productId: UUID,
+    val quantityDelta: Int,
+)
+
+data class AdjustPriceOfItemInCart(
+    @TargetAggregateIdentifier val cartId: UUID,
+    val productId: UUID,
+    val newPrice: BigDecimal,
+)
 data class RemoveItemFromCart(
     @TargetAggregateIdentifier val cartId: UUID,
-    val itemId: UUID,
-    val quantity: Int,
+    val productId: UUID,
 )
 
 data class PayForCart(
     @TargetAggregateIdentifier val cartId: UUID,
-    val amountPaid: Double,
+    val amountPaid: BigDecimal,
 )
--- src/main/kotlin/com/example/cqrs_demo/command/api/events.kt
+++ src/main/kotlin/com/example/cqrs_demo/command/api/events.kt
@@ -1,5 +1,6 @@
 package com.example.cqrs_demo.command.api
 
+import java.math.BigDecimal
 import java.util.*
 
 data class CartCreated(
@@ -8,21 +9,48 @@ data class CartCreated(
 
 data class ItemAddedToCart(
     val cartId: UUID,
-    val itemId: UUID,
+    val productId: UUID,
     val quantity: Int,
+    val basePrice: BigDecimal,
 )
 
 data class ItemRemovedFromCart(
     val cartId: UUID,
-    val itemId: UUID,
-    val quantity: Int,
+    val productId: UUID,
+)
+
+data class TotalAmountRecalculated(
+    val cartId: UUID,
+    val totalAmount: BigDecimal,
+)
+
+data class ItemQuantityAdjustedInCart(
+    val cartId: UUID,
+    val productId: UUID,
+    val quantityDelta: Int,
+)
+
+data class PriceOfItemInCartAdjusted(
+    val cartId: UUID,
+    val productId: UUID,
+    val newPrice: BigDecimal,
 )
 
 data class OrderCreated(
     val cartId: UUID,
+    val orderId: UUID,
+    val orderLines: List<OrderLine>,
+    val totalAmount: BigDecimal,
 )
 
-data class OrderFulfilled(
-    val cartId: UUID,
-    val amountPaid: Double,
+data class OrderLine(
+    val productId: UUID,
+    val quantity: Int,
+    val unitPrice: BigDecimal,
 )
+
+data class PaymentReceived(
+    val cartId: UUID,
+    val amountPaid: BigDecimal,
+)
+

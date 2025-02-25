b317382efeb7748d11c3bd9e23f224f3ddca2273
Haagsma <y.haagsma@portofrotterdam.com>
2025-02-25 15:55:05 +0100

    WIP

--- src/main/kotlin/com/example/cqrs_demo/rest/CheckoutController.kt
+++ src/main/kotlin/com/example/cqrs_demo/rest/CheckoutController.kt
@@ -8,32 +8,34 @@ import community.flock.wirespec.generated.java.CreateCartEndpoint
 import community.flock.wirespec.generated.java.Error
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
 import java.math.BigDecimal
 import java.util.*
-import java.util.concurrent.CompletableFuture
 
 @RestController
-class CheckoutController (val commandGateway: CommandGateway) : CreateCartEndpoint.Handler, AddItemToCartEndpoint.Handler
+class CheckoutController (val commandGateway: CommandGateway, val wirespecWebClient: WebClient
+) : CreateCartEndpoint.Handler, AddItemToCartEndpoint.Handler
 {
-    override fun createCart(request: CreateCartEndpoint.Request?): CompletableFuture<CreateCartEndpoint.Response<*>> {
+    override suspend fun createCart(request: CreateCartEndpoint.Request): CreateCartEndpoint.Response<*> {
         val cartId = UUID.randomUUID()
         commandGateway.send<CreateCart>(CreateCart(cartId))
-        return CompletableFuture.completedFuture(CreateCartEndpoint.Response200(CartId(cartId.toString())))
+        return CreateCartEndpoint.Response200(CartId(cartId.toString()))
     }
 
-    override fun addItemToCart(request: AddItemToCartEndpoint.Request?): CompletableFuture<AddItemToCartEndpoint.Response<*>> {
+    override suspend fun addItemToCart(request: AddItemToCartEndpoint.Request): AddItemToCartEndpoint.Response<*> {
         println("Incoming request body : ${request?.body}")
-        println("request?.body?.cartid?.value() : ${request?.body?.cartid?.value()}")
+        println("request?.body?.cartid?.value() : ${request?.body?.cartid?.value}")
+
         request?.body?.let { addItemToCartRequest ->
             commandGateway.send<AddItemToCart>(
                 AddItemToCart(
-                    UUID.fromString(addItemToCartRequest.cartid.value()),
-                    UUID.fromString(addItemToCartRequest.productid.value()),
+                    UUID.fromString(addItemToCartRequest.cartid.value),
+                    UUID.fromString(addItemToCartRequest.productid.value),
                     addItemToCartRequest.quantity.toInt(),
                     BigDecimal(addItemToCartRequest.baseprice)
                 ))
-                return CompletableFuture.completedFuture(AddItemToCartEndpoint.Response200(addItemToCartRequest.cartid))
+                return AddItemToCartEndpoint.Response200(addItemToCartRequest.cartid)
         }
-        return CompletableFuture.completedFuture(AddItemToCartEndpoint.Response404(Error("404", Optional.of("Item not found"))))
+        return AddItemToCartEndpoint.Response404(Error("404", "Item not found"))
     }
 }

5fc69e9be2147ba4e9b2a8e9569d46e5a9d1d0d6
Haagsma <y.haagsma@portofrotterdam.com>
2025-02-25 16:45:27 +0100

    WIP

--- src/main/kotlin/com/example/cqrs_demo/rest/CheckoutController.kt
+++ src/main/kotlin/com/example/cqrs_demo/rest/CheckoutController.kt
@@ -5,37 +5,33 @@ import com.example.cqrs_demo.command.api.CreateCart
 import community.flock.wirespec.generated.java.AddItemToCartEndpoint
 import community.flock.wirespec.generated.java.CartId
 import community.flock.wirespec.generated.java.CreateCartEndpoint
-import community.flock.wirespec.generated.java.Error
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.client.WebClient
 import java.math.BigDecimal
 import java.util.*
 
 @RestController
-class CheckoutController (val commandGateway: CommandGateway, val wirespecWebClient: WebClient
-) : CreateCartEndpoint.Handler, AddItemToCartEndpoint.Handler
-{
+class CheckoutController(val commandGateway: CommandGateway) : CreateCartEndpoint.Handler,
+    AddItemToCartEndpoint.Handler {
     override suspend fun createCart(request: CreateCartEndpoint.Request): CreateCartEndpoint.Response<*> {
-        val cartId = UUID.randomUUID()
-        commandGateway.send<CreateCart>(CreateCart(cartId))
-        return CreateCartEndpoint.Response200(CartId(cartId.toString()))
+        val cartId = request.body.cartid.value
+        println("Incoming create cart request with id: : $cartId")
+        commandGateway.send<CreateCart>(CreateCart(UUID.fromString(cartId)))
+        return CreateCartEndpoint.Response200(CartId(cartId))
     }
 
     override suspend fun addItemToCart(request: AddItemToCartEndpoint.Request): AddItemToCartEndpoint.Response<*> {
-        println("Incoming request body : ${request?.body}")
-        println("request?.body?.cartid?.value() : ${request?.body?.cartid?.value}")
+        val addItemToCart = request.body
+        println("Incoming request body : $addItemToCart")
 
-        request?.body?.let { addItemToCartRequest ->
-            commandGateway.send<AddItemToCart>(
-                AddItemToCart(
-                    UUID.fromString(addItemToCartRequest.cartid.value),
-                    UUID.fromString(addItemToCartRequest.productid.value),
-                    addItemToCartRequest.quantity.toInt(),
-                    BigDecimal(addItemToCartRequest.baseprice)
-                ))
-                return AddItemToCartEndpoint.Response200(addItemToCartRequest.cartid)
-        }
-        return AddItemToCartEndpoint.Response404(Error("404", "Item not found"))
+        commandGateway.send<AddItemToCart>(
+            AddItemToCart(
+                UUID.fromString(addItemToCart.cartid.value),
+                UUID.fromString(addItemToCart.productid.value),
+                addItemToCart.quantity.toInt(),
+                BigDecimal(addItemToCart.baseprice)
+            )
+        )
+        return AddItemToCartEndpoint.Response200(addItemToCart.cartid)
     }
 }

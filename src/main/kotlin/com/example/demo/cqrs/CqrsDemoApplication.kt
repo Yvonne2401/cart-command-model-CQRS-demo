449c7f413ca42eb77b8587907f76e4841f19f10f
Yvonne Haagsma <yvonne.haagsma@flock.community>
2025-03-10 12:49:22 +0100

    rebase

--- src/main/kotlin/com/example/demo/cqrs/CqrsDemoApplication.kt
+++ src/main/kotlin/com/example/demo/cqrs/CqrsDemoApplication.kt
@@ -1,19 +1,11 @@
 package com.example.demo.cqrs
 
-import community.flock.wirespec.integration.spring.kotlin.configuration.EnableWirespec
-<<<<<<< HEAD:src/main/kotlin/com/example/demo/cqrs/CqrsDemoApplication.kt
 import community.flock.wirespec.integration.spring.kotlin.configuration.EnableWirespecController
-=======
->>>>>>> 3a2841c (WI):src/main/kotlin/com/example/cqrs_demo/CqrsDemoApplication.kt
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-<<<<<<< HEAD:src/main/kotlin/com/example/demo/cqrs/CqrsDemoApplication.kt
 @EnableWirespecController
-=======
-@EnableWirespec
->>>>>>> 3a2841c (WI):src/main/kotlin/com/example/cqrs_demo/CqrsDemoApplication.kt
 class CqrsDemoApplication
 
 fun main(args: Array<String>) {
